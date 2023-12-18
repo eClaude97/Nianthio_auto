86ab52df7d724d2410dc33cf8a0fa26eb8d2b4b3
Claude097 <153725129+eClaude97@users.noreply.github.com>
2023-12-18 16:42:10 +0000

    Nianthio_auto Vesrion 2023-18-12/4:41pm

--- Class/config.php
+++ Class/config.php
@@ -1,7 +1,10 @@
 <?php
+require_once "../vendor/autoload.php";
 
-Const DB_CONNECTION = 'mysql';
-const DB_HOST = 'localhost';
-const DB_DATABASE = 'nianthio_db';
-const DB_PASS = 'root';
-const DB_USER = 'root';
+$env = parse_ini_file("../.env");
+
+define("DB_CONNECTION", $env['DB_CONNECTION']);
+define("DB_HOST", $env['DB_HOST']);
+define("DB_DATABASE", $env['DB_DATABASE']);
+define("DB_PASS", $env['DB_PASS']);
+define("DB_USER", $env['DB_USER']);
